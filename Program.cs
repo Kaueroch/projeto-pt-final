1b48a3aef1fd49f319b43244c0e5aa2f54a5aaff
Kauê Alcantara Rocha <118750523+garotodeprogramacao@users.noreply.github.com>
2023-12-02 14:37:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+
+class Program
+{
+    static List<string> anotacoes = new List<string>();
+
+    static void Perguntar()
+    {
+        do
+        {
+            // Seu código para realizar contas aqui
+
+            Console.WriteLine("Deseja continuar realizando contas? (S/N): ");
+            string continuar = Console.ReadLine();
+            if (continuar.ToUpper() == "S")
+            {
+                Console.WriteLine("Qual dessas operações você quer?");
+                Console.WriteLine("1 - Adição");
+                Console.WriteLine("2 - Subtração");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
+                int escolha = Convert.ToInt32(Console.ReadLine());
+                switch (escolha)
+                {
+                    case 1:
+                        Console.WriteLine("Adição");
+                        Console.WriteLine("-----------------------");
+                        Console.WriteLine("Digite um numero:");
+                        int n1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Digite outro numero:");
+                        int n2 = Convert.ToInt32(Console.ReadLine());
+                        int resultado = n1 + n2;
+                        Console.WriteLine(" O resultado é: " + resultado);
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Subtração");
+                        Console.WriteLine("-----------------------");
+                        Console.WriteLine("Digite um numero:");
+                        n1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Digite outro numero:");
+                        n2 = Convert.ToInt32(Console.ReadLine());
+                        resultado = n1 - n2;
+                        Console.WriteLine(" O resultado é: " + resultado);
+
+                        break;
+                    case 3:
+                        Console.WriteLine("Multiplicação");
+                        Console.WriteLine("-----------------------");
+                        Console.WriteLine("Digite um numero:");
+                        n1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Digite outro numero:");
+                        n2 = Convert.ToInt32(Console.ReadLine());
+                        resultado = n1 * n2;
+                        Console.WriteLine(" O resultado é: " + resultado);
+
+                        break;
+                    case 4:
+                        Console.WriteLine("DIvisão");
+                        Console.WriteLine("-----------------------");
+                        Console.WriteLine("Digite um numero:");
+                        n1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Digite outro numero:");
+                        n2 = Convert.ToInt32(Console.ReadLine());
+                        resultado = n1 / n2;
+                        Console.WriteLine(" O resultado é: " + resultado);
+                        break;
+                }
+            }
+
+            if (continuar.ToUpper() == "N")
+            {
+                Console.WriteLine("Encerrando o programa. Até mais!");
+                break;
+            }
+
+        } while (true);
+
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Qual dessas opções você quer?");
+        Console.WriteLine("1 - Calculadora");
+        Console.WriteLine("2 - Anotações");
+        Console.WriteLine("3 - Mega-sena");
+        Console.WriteLine("4 - Sair");
+
+        int escolha = Convert.ToInt32(Console.ReadLine());
+
+        switch (escolha)
+
+
+        //Tentar criar um método para voltar ao começo do menu de operação ;-
+        //tentar criar outro metodo com o mesmo switch e depois chamar la embaixo, é uma gambiarra mas deve funcionar
+
+        {
+            case 1:
+                Console.WriteLine("Qual dessas operações você quer?");
+                Console.WriteLine("1 - Adição");
+                Console.WriteLine("2 - Subtração");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
+
+
+
+                int escolhacalc = Convert.ToInt32(Console.ReadLine());
+                double n1, n2, resultado = 0;
+                switch (escolhacalc)
+                {
+                    case 1:
+                        Console.WriteLine("Adição");
+                        Console.WriteLine("-----------------------");
+                        Console.WriteLine("Digite um numero:");
+                        n1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Digite outro numero:");
+                        n2 = Convert.ToInt32(Console.ReadLine());
+                        resultado = n1 + n2;
+                        Console.WriteLine(" O resultado é: " + resultado);
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Subtração");
+                        Console.WriteLine("-----------------------");
+                        Console.WriteLine("Digite um numero:");
+                        n1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Digite outro numero:");
+                        n2 = Convert.ToInt32(Console.ReadLine());
+                        resultado = n1 - n2;
+                        Console.WriteLine(" O resultado é: " + resultado);
+
+                        break;
+                    case 3:
+                        Console.WriteLine("Multiplicação");
+                        Console.WriteLine("-----------------------");
+                        Console.WriteLine("Digite um numero:");
+                        n1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Digite outro numero:");
+                        n2 = Convert.ToInt32(Console.ReadLine());
+                        resultado = n1 * n2;
+                        Console.WriteLine(" O resultado é: " + resultado);
+
+                        break;
+                    case 4:
+                        Console.WriteLine("DIvisão");
+                        Console.WriteLine("-----------------------");
+                        Console.WriteLine("Digite um numero:");
+                        n1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Digite outro numero:");
+                        n2 = Convert.ToInt32(Console.ReadLine());
+                        resultado = n1 / n2;
+                        Console.WriteLine(" O resultado é: " + resultado);
+                        break;
+
+
+
+                }
+                Perguntar();
+                //calculadora pronta!
+                break;
+            case 2:
+                //anotações
+                do
+                {
+
+                    Console.WriteLine("Digite uma anotação");
+                    string anotação = Console.ReadLine();
+                    anotacoes.Add(anotação);
+                    Console.WriteLine("Adicionado com sucesso");
+                    Console.WriteLine("Anotações:");
+                    foreach (var anotacao in anotacoes)
+                    {
+                        Console.WriteLine(anotacao);
+                    }
+                    Console.WriteLine("Deseja continuar realizando contas? (S/N): ");
+                    string continuar = Console.ReadLine();
+                    if (continuar.ToUpper() == "N")
+                    {
+                        Console.WriteLine("Saindo até mais!!");
+                        break;
+                    }
+                } while (true);
+                //anotações pronta!         
+                break;
+
+            case 3:
+            
+                Console.WriteLine("Escolheu Mega sena!!");
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Venha testar sua sorte!");
+                Console.WriteLine("Digite um numero!");
+                n1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite um numero!");
+                n2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite um numero!");
+                int n3 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite um numero!");
+                int n4 = Convert.ToInt32(Console.ReadLine());
+                Random rnd = new Random();
+                 int sorteio1 = rnd.Next(1, 100);
+                int sorteio2 = rnd.Next(1, 100);
+                int sorteio3 = rnd.Next(1, 100);
+                int sorteio4 = rnd.Next(1, 100);
+                if (n1 == sorteio1 && n2 == sorteio2 && n3 == sorteio3 && n4 == sorteio4)
+                {
+                    Console.WriteLine("Parabens!! Voce ganhou a mega-sena!!" + n1 + n2 + n3 + n4);
+                }else{
+                    Console.WriteLine("Não foi dessa vez!):");
+                }
+                Console.WriteLine("Saindo!! até mais!");
+                break;       
+            case 4:
+                Console.WriteLine("Saindo! Até mais!");
+                break;
+        }
+
+        //projeto concluido
+    }
+}
+
+
